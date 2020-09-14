e629a54539495f86519357afb3c2061fbdd66648
Produgy <produgy@rajeshs-mbp.home>
2020-09-14 20:48:51 +0100

    delete

--- under-construction-page-master/gulpfile.js
+++
@@ -1,8 +0,0 @@
-var gulp = require('gulp');
-var cssminify = require('gulp-minify-css');
-
-gulp.task('default', function() {
-  gulp.src('css/colors*.css')
-  .pipe(cssminify())
-  .pipe(gulp.dest('dist'));
-});
